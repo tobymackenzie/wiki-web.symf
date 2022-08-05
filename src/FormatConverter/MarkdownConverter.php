e9f252fae9aa2250d3ffef76669a3bf3ee7a5242
Toby Mackenzie <public@tobymackenzie.com>
2022-08-05 19:15:27 -0400

    code: use markdown converter interface to allow greater configurability

--- src/FormatConverter/MarkdownConverter.php
+++ src/FormatConverter/MarkdownConverter.php
@@ -1,10 +1,11 @@
 <?php
 namespace TJM\WikiWeb\FormatConverter;
 use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\ConverterInterface as LeagueConverterInterface;
 
 class MarkdownConverter implements ConverterInterface{
 	protected $converter;
-	public function __construct(CommonMarkConverter $converter = null){
+	public function __construct(LeagueConverterInterface $converter = null){
 		$this->converter = $converter ?? new CommonMarkConverter();
 	}
 	public function supports(string $from, string $to){
